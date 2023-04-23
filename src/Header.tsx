52c6360cd60f34f521117b00036b6351665dbc11
AouIssa <spunkyguyyt@gmail.com>
2023-04-23 23:57:08 +0800

    Updated to have IOS header

--- src/Header.tsx
+++ src/Header.tsx
@@ -8,22 +8,21 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ title, onBack }) => {
   return (
-<header className="bg-white shadow p-4 mt-5 block md:hidden">
-  <div className="container mx-auto flex items-center justify-between">
-    {onBack ? (
-      <button className="focus:outline-none" onClick={onBack}>
-        <IoIosArrowBack className="text-blue-500" size={28} />
-      </button>
-    ) : (
-      <div className="w-20" />
-    )}
-    <div className="w-full text-center">
-      <h1 className="text-lg font-semibold">{title}</h1>
-    </div>
-    <div className="w-10" />
-  </div>
-</header>
-
+    <header className="bg-white shadow p-2 mt-3 block md:hidden">
+      <div className="container mx-auto flex items-center justify-between">
+        {onBack ? (
+          <button className="focus:outline-none" onClick={onBack}>
+            <IoIosArrowBack className="text-blue-500" size={28} />
+          </button>
+        ) : (
+          <div className="w-20" />
+        )}
+        <div className="w-full text-center">
+          <h1 className="text-lg font-sans text-gray-500 font-bold">{title}</h1>
+        </div>
+        <div className="w-10" />
+      </div>
+    </header>
   );
 };
 
