e3964a54fc94b0d689eff57ab48924513b50eaf7
AouIssa <spunkyguyyt@gmail.com>
2023-04-24 02:25:32 +0800

    Table Ui fixes (In progress)

--- src/PaymentTable/PaymentTable.tsx
+++ src/PaymentTable/PaymentTable.tsx
@@ -25,106 +25,112 @@ const PaymentTable: React.FC = () => {
 
   return (
     <div className="relative w-full">
-      <div className="text-2xl font-semibold mb-6 text-gray-600 w-full pb-2 border-b border-gray-200 hidden md:block">
-        Payments
-      </div>
-      <div className="w-full mb-6">
-        <label htmlFor="fee-type-select" className="mr-2">
-          Filter by fee type:
-        </label>
-        <select
-          id="fee-type-select"
-          onChange={(e) => setSelectedFeeType(e.target.value)}
-          value={selectedFeeType ?? ""}
-          className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-        >
-          <option value="">All fee types</option>
-          <option value="Team fee">Team fee</option>
-          <option value="Spare Fee">Spare Fee</option>
-          <option value="Jersey Fee">Jersey Fee</option>
-          <option value="Drink">Drink</option>
-          <option value="Tournament">Tournament</option>
-          <option value="Event">Event</option>
-          <option value="Other">Other</option>
-        </select>
-      </div>
-      <div className="w-full overflow-x-auto">
-        <table className="min-w-full divide-y divide-gray-200 border border-gray-300 rounded-lg">
-          <thead className="bg-gray-50">
-            <tr>
-              <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Fee Type
-              </th>
-              <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Fee
-              </th>
-              <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Amount Due
-              </th>
-              <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Date Due
-              </th>
-              <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Paid
-              </th>
-              <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
-                Remaining
-              </th>
-              <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200">
-            {filteredPayments.length === 0 ? (
+      <div className="border border-gray-300 p-4 rounded-lg">
+        <div className="text-2xl font-semibold mb-6 text-gray-600 w-full pb-2 border-b border-gray-200 hidden md:block">
+          Payments
+        </div>
+        <div className="w-full mb-6">
+          <label htmlFor="fee-type-select" className="mr-2">
+            Filter by fee type:
+          </label>
+          <select
+            id="fee-type-select"
+            onChange={(e) => setSelectedFeeType(e.target.value)}
+            value={selectedFeeType ?? ""}
+            className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+          >
+            <option value="">All fee types</option>
+            <option value="Team fee">Team fee</option>
+            <option value="Spare Fee">Spare Fee</option>
+            <option value="Jersey Fee">Jersey Fee</option>
+            <option value="Drink">Drink</option>
+            <option value="Tournament">Tournament</option>
+            <option value="Event">Event</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
+        <div className="w-full overflow-x-auto">
+          <table className="min-w-full divide-y divide-gray-200 rounded-lg">
+            <thead className="bg-gray-50">
               <tr>
-                <td
-                  colSpan={7}
-                  className="px-6 py-4 whitespace-nowrap text-center"
-                >
-                  <div className="text-sm text-gray-900">No payments due</div>
-                </td>
+                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Fee Type
+                </th>
+                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Fee
+                </th>
+                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Amount Due
+                </th>
+                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Date Due
+                </th>
+                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Paid
+                </th>
+                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                  Remaining
+                </th>
+                <th className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"></th>
               </tr>
-            ) : (
-              filteredPayments.map((payment, index) => (
-                <tr key={index}>
-                  <td className="px-6 py-4 whitespace-nowrap text-center">
-                    <div className="text-sm text-gray-900">
-                      {payment.feeType}
-                    </div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-center">
-                    <div className="text-sm text-gray-900">${payment.fee}</div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-center">
-                    <div className="text-sm text-gray-900">
-                      ${payment.amountDue}
-                    </div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-center">
-                    <div className="text-sm text-gray-900">
-                      {payment.dateDue}
-                    </div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-center">
-                    <div className="text-sm text-gray-900">${payment.paid}</div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-center">
-                    <div className="text-sm text-gray-900">
-                      ${payment.remaining}
-                    </div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-center">
-                    <button
-                      className="px-4 py-2 text-sm font-medium text-white bg-green-500 rounded hover:bg-green-600 focus:outline-none"
-                      onClick={() => handlePayment(payment)}
-                    >
-                      Pay
-                    </button>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              {filteredPayments.length === 0 ? (
+                <tr>
+                  <td
+                    colSpan={7}
+                    className="px-6 py-4 whitespace-nowrap text-center"
+                  >
+                    <div className="text-sm text-gray-900">No payments due</div>
                   </td>
                 </tr>
-              ))
-            )}
-          </tbody>
-        </table>
+              ) : (
+                filteredPayments.map((payment, index) => (
+                  <tr key={index}>
+                    <td className="px-6 py-4 whitespace-nowrap text-center">
+                      <div className="text-sm text-gray-900">
+                        {payment.feeType}
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-center">
+                      <div className="text-sm text-gray-900">
+                        ${payment.fee}
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-center">
+                      <div className="text-sm text-gray-900">
+                        ${payment.amountDue}
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-center">
+                      <div className="text-sm text-gray-900">
+                        {payment.dateDue}
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-center">
+                      <div className="text-sm text-gray-900">
+                        ${payment.paid}
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-center">
+                      <div className="text-sm text-gray-900">
+                        ${payment.remaining}
+                      </div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-center">
+                      <button
+                        className="px-4 py-2 text-sm font-medium text-white bg-green-500 rounded hover:bg-green-600 focus:outline-none"
+                        onClick={() => handlePayment(payment)}
+                      >
+                        Pay
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
