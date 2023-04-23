77d7f06ba211ed96000d3705a1f16fafd06dbd5a
AouIssa <spunkyguyyt@gmail.com>
2023-04-23 21:33:01 +0800

    Moved payment table and its data file into same folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./App.css";
-import PaymentTable from "./PaymentTable";
+import PaymentTable from "./PaymentTable/PaymentTable";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-   
       <main className="p-4">
         <PaymentTable />
       </main>
